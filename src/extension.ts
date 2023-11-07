d37298764fe54de98a4ccf705f1f32bb5ca1f12b
Mengyan Wu <mengyanw@umich.edu>
2023-11-07 16:16:04 -0500

    feat: run prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,87 +1,101 @@
-import * as vscode from 'vscode'
-import { writeToInfluxDB } from './influx'
+import * as vscode from "vscode";
+import { writeToInfluxDB } from "./influx";
 
-let nextId = 1
-let uri2Id: { [key: string]: number } = {}
+let nextId = 1;
+let uri2Id: { [key: string]: number } = {};
 
 function getId(uri: string) {
-	if (!uri2Id.hasOwnProperty(uri)) uri2Id[uri] = nextId++
-	return uri2Id[uri]
+  if (!uri2Id.hasOwnProperty(uri)) uri2Id[uri] = nextId++;
+  return uri2Id[uri];
 }
 
 // send entire text document content & update megaphone
-function sendDoc(document: vscode.TextDocument, megaphone?: vscode.StatusBarItem) {
-	if (document.uri.scheme === 'output') {
-		return; // To avoid logging output panel content
-	}
+function sendDoc(
+  document: vscode.TextDocument,
+  megaphone?: vscode.StatusBarItem,
+) {
+  if (document.uri.scheme === "output") {
+    return; // To avoid logging output panel content
+  }
 
-	const id = getId(document.uri.toString())
-	const doc = document.getText()
+  const id = getId(document.uri.toString());
+  const doc = document.getText();
 
-	if (megaphone) {
-		megaphone.text = `$(megaphone) Document ${id} (${document.fileName}) Open: ${doc.split('\n')[0]}`
-	}
+  if (megaphone) {
+    megaphone.text = `$(megaphone) Document ${id} (${
+      document.fileName
+    }) Open: ${doc.split("\n")[0]}`;
+  }
 
-	writeToInfluxDB('doc', doc, id.toString())
+  writeToInfluxDB("doc", doc, id.toString());
 }
 
 // send text document changes & update megaphone
-function sendDocChange(event: vscode.TextDocumentChangeEvent, megaphone: vscode.StatusBarItem) {
-	if (event.document.uri.scheme === 'output') {
-		return; // To avoid logging output panel content
-	}
+function sendDocChange(
+  event: vscode.TextDocumentChangeEvent,
+  megaphone: vscode.StatusBarItem,
+) {
+  if (event.document.uri.scheme === "output") {
+    return; // To avoid logging output panel content
+  }
 
-	const id = getId(event.document.uri.toString())
+  const id = getId(event.document.uri.toString());
 
-	// For dashboard
-	// To mirror the content of a document using change events use the following
-	// approach:
-	// - start with the same initial content
-	// - apply the 'textDocument/didChange' notifications in the order you
-	// 	 receive them.
-	// - apply the `TextDocumentContentChangeEvent`s in a single notification
-	//   in the order you receive them.
-	writeToInfluxDB('docChange', JSON.stringify(event.contentChanges), id.toString())
+  // For dashboard
+  // To mirror the content of a document using change events use the following
+  // approach:
+  // - start with the same initial content
+  // - apply the 'textDocument/didChange' notifications in the order you
+  // 	 receive them.
+  // - apply the `TextDocumentContentChangeEvent`s in a single notification
+  //   in the order you receive them.
+  writeToInfluxDB(
+    "docChange",
+    JSON.stringify(event.contentChanges),
+    id.toString(),
+  );
 
-	// For megaphone
-	if (event.contentChanges.length === 0) {
-		megaphone.text = (`$(megaphone) Document ${id} Save`)
-	}
-	event.contentChanges.forEach((change) => {
-		if (change && change.hasOwnProperty('text')) {
-			const { text, range } = change
-			if (range.start.isEqual(range.end)) {
-				megaphone.text = `$(megaphone) Document ${id} Add: ${text}`
-			} else if (text === '') {
-				megaphone.text = `$(megaphone) Document ${id} Delete`
-			} else {
-				megaphone.text = `$(megaphone) Document ${id} Replace: ${text}`
-			}
-		}
-	})
+  // For megaphone
+  if (event.contentChanges.length === 0) {
+    megaphone.text = `$(megaphone) Document ${id} Save`;
+  }
+  event.contentChanges.forEach((change) => {
+    if (change && change.hasOwnProperty("text")) {
+      const { text, range } = change;
+      if (range.start.isEqual(range.end)) {
+        megaphone.text = `$(megaphone) Document ${id} Add: ${text}`;
+      } else if (text === "") {
+        megaphone.text = `$(megaphone) Document ${id} Delete`;
+      } else {
+        megaphone.text = `$(megaphone) Document ${id} Replace: ${text}`;
+      }
+    }
+  });
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Extension "telemetry" is now active')
-	console.log(`sessionId: ${vscode.env.sessionId}`)
+  console.log('Extension "telemetry" is now active');
+  console.log(`sessionId: ${vscode.env.sessionId}`);
 
-	// create megaphone item on the status bar
-	let megaphone = vscode.window.createStatusBarItem(
-		vscode.StatusBarAlignment.Right,
-		100
-	)
-	megaphone.text = `$(megaphone) telemetry extension is activated`
-	megaphone.show()
+  // create megaphone item on the status bar
+  let megaphone = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100,
+  );
+  megaphone.text = `$(megaphone) telemetry extension is activated`;
+  megaphone.show();
 
-	// send all text document contents currently known to the editor
-	vscode.workspace.textDocuments.forEach((doc) => sendDoc(doc))
+  // send all text document contents currently known to the editor
+  vscode.workspace.textDocuments.forEach((doc) => sendDoc(doc));
 
-	// add listeners for document open and document change
-	context.subscriptions.push(
-		megaphone,
-		vscode.workspace.onDidOpenTextDocument((doc) => sendDoc(doc, megaphone)),
-		vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => sendDocChange(e, megaphone))
-	)
+  // add listeners for document open and document change
+  context.subscriptions.push(
+    megaphone,
+    vscode.workspace.onDidOpenTextDocument((doc) => sendDoc(doc, megaphone)),
+    vscode.workspace.onDidChangeTextDocument(
+      (e: vscode.TextDocumentChangeEvent) => sendDocChange(e, megaphone),
+    ),
+  );
 }
 
-export function deactivate() { }
+export function deactivate() {}
--- src/influx.ts
+++ src/influx.ts
@@ -1,17 +1,21 @@
-import * as vscode from 'vscode'
-import { InfluxDB, Point } from '@influxdata/influxdb-client'
-import { org, token, url } from './secrets'
+import * as vscode from "vscode";
+import { InfluxDB, Point } from "@influxdata/influxdb-client";
+import { org, token, url } from "./secrets";
 
-const bucket = 'vs-dev'
-const influxDB = new InfluxDB({ url, token })
+const bucket = "vs-dev";
+const influxDB = new InfluxDB({ url, token });
 
-export async function writeToInfluxDB(eventType: string, eventInfo: string, docId: string) {
-    const writeApi = influxDB.getWriteApi(org, bucket)
-    writeApi.useDefaultTags({ from: 'vscode extension' })
-    const dataPoint = new Point('telemetry')
-        .stringField('eventType', eventType)
-        .stringField('eventInfo', eventInfo)
-        .tag('sessionId', vscode.env.sessionId)
-        .tag('docId', docId)
-    writeApi.writePoint(dataPoint)
+export async function writeToInfluxDB(
+  eventType: string,
+  eventInfo: string,
+  docId: string,
+) {
+  const writeApi = influxDB.getWriteApi(org, bucket);
+  writeApi.useDefaultTags({ from: "vscode extension" });
+  const dataPoint = new Point("telemetry")
+    .stringField("eventType", eventType)
+    .stringField("eventInfo", eventInfo)
+    .tag("sessionId", vscode.env.sessionId)
+    .tag("docId", docId);
+  writeApi.writePoint(dataPoint);
 }
