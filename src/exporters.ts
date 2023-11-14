002bf0903be727ea04f1f1d8b46aa65f52146cf6
Mengyan Wu <mengyanw@umich.edu>
2023-11-14 14:48:22 -0500

    feat: file exporter

--- src/exporters.ts
+++ src/exporters.ts
@@ -1,10 +1,37 @@
 import { EventData, Exporter, ExporterArgs } from "./types";
 import * as vscode from "vscode";
+import * as fs from "fs";
+import { posix } from "path";
 
 function consoleExporter(data: EventData) {
   console.log(data);
 }
+async function fileExporter(data: EventData, args: ExporterArgs | undefined) {
+  if (args && args.path && vscode.workspace.workspaceFolders) {
+    const writeStr = JSON.stringify(data) + ",";
+    const writeData = Buffer.from(writeStr, "utf8");
 
+    const folderUri = vscode.workspace.workspaceFolders[0].uri;
+    const fileUri = posix.join(folderUri.path, args.path);
+    // VS code extension only support writing to relative path.
+
+    if (fileUri.toString() !== vscode.Uri.parse(data.documentUri).path) {
+      try {
+        fs.appendFileSync(fileUri, writeData);
+        return {
+          exporter: args.id || "FileExporter",
+          message: "Success",
+        };
+      } catch (err) {
+        console.log(err);
+        return {
+          exporter: args.id || "FileExporter",
+          message: "Failed to write to file",
+        };
+      }
+    }
+  }
+}
 async function remoteExporter(data: EventData, args: ExporterArgs | undefined) {
   if (args && args.url) {
     const headers: Headers = new Headers();
@@ -17,7 +44,7 @@ async function remoteExporter(data: EventData, args: ExporterArgs | undefined) {
       body: JSON.stringify({
         data: data,
         params: args.params,
-        env: args.env
+        env: args.env,
       }),
     });
     const response = await fetch(request);
@@ -31,15 +58,21 @@ async function remoteExporter(data: EventData, args: ExporterArgs | undefined) {
     };
   }
 }
-export function sendEvent(data: EventData) {
+export async function sendEvent(data: EventData) {
   const exporters = vscode.workspace
     .getConfiguration("telemetry")
     .get("exporters") as Array<Exporter>;
-  exporters.forEach(async (exporter: Exporter) => {
+  const results: any = [];
+  await exporters.forEach(async (exporter: Exporter) => {
     if (exporter.type === "console_exporter") consoleExporter(data);
+    if (exporter.type === "file_exporter") {
+      const response = await fileExporter(data, exporter.args);
+      results.push(response);
+    }
     if (exporter.type === "remote_exporter") {
-      const res = await remoteExporter(data, exporter.args);
-      console.log(res);
+      const response = await remoteExporter(data, exporter.args);
+      results.push(response);
     }
   });
+  console.log(results);
 }
